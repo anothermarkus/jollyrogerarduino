dc0ba3e553a352a9901cf02d150efaa15bd2faeb
Ryuu <ryuu.kopec@outlook.com>
2025-09-21 14:24:58 -0500

    Yo Ho Song Complete

--- TalkingJollyRoger.ino
+++ TalkingJollyRoger.ino
@@ -29,38 +29,53 @@ const int JAW_1      = 30;
 const int JAW_CLOSED = 45; 
 
 // -------------------- Phrase arrays --------------------
-// Phrase 1: "Yo ho, yo ho, a pirate's life for me"
+// "Yo ho, yo ho, a pirate's life for me"
 const int yohoMovements[] = { JAW_OPEN, JAW_OPEN, JAW_OPEN, JAW_OPEN, JAW_4, JAW_3, JAW_3, JAW_3, JAW_3, JAW_OPEN };
 const int yoHoDelays[]    = { 450, 450, 450, 450, 350, 350, 250, 350, 250, 550 };
 
-// Phrase 2: "We pillage plunder, we rifle and loot"
+// "We pillage plunder, we rifle and loot"
 const int pillageMovements[] = { JAW_4, JAW_3, JAW_OPEN, JAW_3, JAW_OPEN, JAW_4, JAW_3, JAW_OPEN, JAW_2, JAW_OPEN };
 const int pillageDelays[]    = { 150, 170, 230, 170, 230, 150, 170, 230, 150, 350 };
 
-// Phrase 3: "Drink up me 'earties, yo ho"
+// "Drink up me 'earties, yo ho"
 const int drinkMovements[] = { JAW_2, JAW_OPEN, JAW_2, JAW_4, JAW_2, JAW_4, JAW_OPEN };
 const int drinkDelays[]    = { 230, 180, 170, 250, 200, 280, 290 };
 
-// Phrase 4: "We kidnap and ravage and don't give a hoot"
+// "We kidnap and ravage and don't give a hoot"
 const int kidnapMovements[] = { JAW_3, JAW_2, JAW_3, JAW_2, JAW_3, JAW_2, JAW_2, JAW_3, JAW_2, JAW_3, JAW_OPEN };
 const int kidnapDelays[]    = { 160, 190, 190, 160, 200, 180, 160, 200, 160, 250, 250 };
 
-// Phrase 5: "We extort and pilfer, we filch and sack"
+// "We extort and pilfer, we filch and sack"
 const int extortMovements[] = { JAW_3, JAW_3,JAW_2, JAW_2, JAW_3, JAW_OPEN, JAW_2, JAW_4,JAW_2, JAW_OPEN };
 const int extortDelays[] = { 180, 260, 200, 170, 260, 200, 180, 270, 170, 150 };
 
-// Phrase 6: "Maraud and embezzle and even highjack"
+// "Maraud and embezzle and even highjack"
 const int maraudMovements[] = { JAW_OPEN, JAW_2, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_2, JAW_OPEN, JAW_3, JAW_OPEN, JAW_2 };  
 const int maraudDelays[]    = { 178, 178, 154, 178, 178, 166, 154, 178, 178, 209, 246 };
 
-// Phrase 7: "We kindle and char and enflame and ignite"
+// "We kindle and char and enflame and ignite"
 const int kindleMovements[] = {JAW_3, JAW_2, JAW_3, JAW_2, JAW_OPEN, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_2, JAW_OPEN, JAW_3};  
 const int kindleDelays[]    = {181, 181, 158, 158, 203, 158, 181, 181, 169, 158, 214, 259};
 
-// Phrase 8: "We burn up the city, we're really a fright"
+// "We burn up the city, we're really a fright"
 const int burnMovements[] = { JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_OPEN }; 
 const int burnDelays[] = {206, 194, 218, 206, 194, 242, 206, 218, 315};
 
+// "We're rascals and scoundrels, we're villains and knaves"
+const int rascalMovements[] = { JAW_3, JAW_2, JAW_3, JAW_2, JAW_3, JAW_OPEN, JAW_3, JAW_2, JAW_3, JAW_2, JAW_OPEN };  
+const int rascalDelays[]    = {190, 180, 190, 170, 200, 260, 180, 190, 200, 170, 270};
+
+// "We're devils and black sheep, we're really bad eggs"
+const int devilsMovements[] = { JAW_3, JAW_2, JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_OPEN };  
+const int devilsDelays[]    = {190, 180, 200, 170, 250, 190, 180, 250, 200, 180, 260};
+
+// "We're beggars and blighters and ne'er do-well cads"
+const int beggarsMovements[] = { JAW_3, JAW_2, JAW_3, JAW_OPEN, JAW_2, JAW_3, JAW_2, JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_OPEN };
+const int beggarsDelays[]    = { 190, 170, 180, 210, 170, 190, 170, 180, 170, 210, 200, 260 };
+
+// "Aye, but we're loved by our mommies and dads"
+const int ayeMovements[] = { JAW_OPEN, JAW_2, JAW_3, JAW_2, JAW_OPEN, JAW_3, JAW_2, JAW_3, JAW_OPEN };
+const int ayeDelays[]    = { 210, 170, 190, 170, 220, 190, 170, 210, 270 };
 
 
 
@@ -174,9 +189,8 @@ void setup() {
   DF1201S.switchFunction(DF1201S.MUSIC);
 }
 
-void loop() {
-  DF1201S.setPlayTime(0);
 
+void playYoHoVerse1(){
   playPhrase(yohoMovements,    yoHoDelays,    sizeof(yohoMovements)/sizeof(int));
   delay(250);
   playPhrase(pillageMovements, pillageDelays, sizeof(pillageMovements)/sizeof(int));
@@ -187,6 +201,9 @@ void loop() {
   delay(250);
   playPhrase(drinkMovements,   drinkDelays,   sizeof(drinkMovements)/sizeof(int));
   delay(750);
+}
+
+void playYoHoVerse2(){
   playPhrase(yohoMovements,    yoHoDelays,    sizeof(yohoMovements)/sizeof(int));
   delay(250);
   playPhrase(extortMovements,    extortDelays,    sizeof(extortMovements)/sizeof(int));
@@ -197,6 +214,9 @@ void loop() {
   delay(100);
   playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
   delay(750);
+}
+
+void playYoHoVerse3(){
   playPhrase(yohoMovements, yoHoDelays, sizeof(yohoMovements)/sizeof(int));
   delay(250);
   playPhrase(kindleMovements, kindleDelays, sizeof(kindleMovements)/sizeof(int));
@@ -206,8 +226,49 @@ void loop() {
   playPhrase(burnMovements, burnDelays, sizeof(burnMovements)/sizeof(int));
   delay(150);
   playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
+  delay(550);
+}
+
+void playYoHoVerse4(){
+  playPhrase(rascalMovements, rascalDelays, sizeof(rascalMovements)/sizeof(int));
+  delay(150);
+  playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
+  delay(150);
+  playPhrase(devilsMovements, devilsDelays, sizeof(devilsMovements)/sizeof(int));
+  delay(150);
+  playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
+  delay(650);
+}
+
+void playYoHoVerse5(){
+  playPhrase(yohoMovements, yoHoDelays, sizeof(yohoMovements)/sizeof(int));
+  delay(350);
+  playPhrase(beggarsMovements, beggarsDelays, sizeof(beggarsMovements)/sizeof(int));
+  delay(50);
+  playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
+  delay(350);
+  playPhrase(ayeMovements, ayeDelays, sizeof(ayeMovements)/sizeof(int));
+  delay(150);
+  playPhrase(drinkMovements, drinkDelays, sizeof(drinkMovements)/sizeof(int));
+  delay(550);
+}
+
+
+void playYoHoSong(){
+  DF1201S.setPlayTime(0);
+
+  playYoHoVerse1();
+  playYoHoVerse2();
+  playYoHoVerse3();
+  playYoHoVerse4();
+  playYoHoVerse5();
 
   mouth.write(JAW_CLOSED);
   DF1201S.pause();
+
+}
+
+void loop() {
+  playYoHoSong();
   delay(10000);
 }
